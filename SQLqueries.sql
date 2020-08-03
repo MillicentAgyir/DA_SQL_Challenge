cd0ad9b145b5e4b1c7a070dd670e30c7fddc9c76
MillicentAgyir <67456943+MillicentAgyir@users.noreply.github.com>
2020-08-03 12:05:41 +0000

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -18,14 +18,21 @@ WHERE when_created BETWEEN '2018-01-01' AND '2018-12-31'
 GROUP BY 2
 ORDER BY 2 ASC;
 
-
-
 5.Select sum(case when amount>0 Then amount else 0 END) as withdrawal,
  sum (case when amount<0 then amount else 0 END ) as deposit,
 case when ((sum(case when amoount > o THEN amount else 0 END)) >((sum(case when amount<0 then amount else 0 END)))*-1)
 then 'withdrawer else 'depositer' END as agent_status, count(*) 
 from agent_transactions WHERE when_created between
-(now()-'1week'::  interval) and now()
+(now()-'1week'::  interval) and now();
+
+6.
+
+7. Select count (agent_transactions.atx_id) as "atx volume city summary",
+    agents.city, agents.country from transactions.agent transactions
+    left join transactions.agents on agent_transactions.agent_id = agents.agent_id
+    where
+    agent_transactions.when_created > = NOW() - INTERVAL '1 week'
+    group by agents.city, agents.country;
 
 
 8. Select transfers.kind As transfer_kind,
@@ -50,11 +57,10 @@ Group by wallets.ledger_location,
 transfers.kind;
 
 
-10. SELECT (source_wallet_id),
-(Select (Sum (send_amount_scalar)) as amount_transferred from transactions.transfers )
+10. SELECT source_wallet_id,
+(Select send_amount_scalar from transactions.transfers where send_amount_scalar = 1000000)
 From transactions.transfers
-WHERE (send_amount_currency = 'CFA' AND transfers.when_created > CURRENT_DATE - INTERVAL '1 month')
-Group by source_wallet_id;
+WHERE (send_amount_currency = 'CFA' AND transfers.when_created =NOW() - INTERVAL '1 month';
 
 
 
